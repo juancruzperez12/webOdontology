296b6b89380058f5e7f7b7e8bfe0a8b24f6b3c94
juancruzperez12 <83PE42956870@campus.economicas.uba.ar>
2025-09-15 18:18:32 -0300

    navbar

---
+++ burger.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("hamburger-menu")
+  .addEventListener("click", function () {
+    var nav = document.querySelector(".top-nav");
+    nav.classList.toggle("show");
+  });
